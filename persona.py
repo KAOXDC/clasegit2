b2141cf7cf125500a5cca581994f45506c9e3665
Diego Prado <kaoxdc@gmail.com>
2024-11-13 09:38:20 -0500

    tercer commit

--- persona.py
+++ persona.py
@@ -7,7 +7,9 @@ class Persona:
 p1 = Persona('Diego', 15)
 p2 = Persona('Ana', 32)
 p3 = Persona('Jose', 52)
+p4 = Persona('Kelly', 25)
 
 print(p1.nombre, p1.edad)
 print(p2.nombre, p2.edad)
 print(p3.nombre, p3.edad)
+print(p4.nombre, p4.edad)
