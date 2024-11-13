2b81158c7b407fd72b9339ee9309e81ac9796853
Diego Prado <kaoxdc@gmail.com>
2024-11-13 09:31:34 -0500

    creamos 3 registros e imprimimos en pantalla

--- persona.py
+++ persona.py
@@ -3,3 +3,11 @@ class Persona:
     def __init__ (self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+
+p1 = Persona('Diego', 15)
+p2 = Persona('Ana', 32)
+p3 = Persona('Jose', 52)
+
+print(p1.nombre, p1.edad)
+print(p2.nombre, p2.edad)
+print(p3.nombre, p3.edad)
