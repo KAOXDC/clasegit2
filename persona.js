45a7057fae9349f05cb7b91549425c75aa4e61cb
Diego Prado <kaoxdc@gmail.com>
2024-11-13 09:52:18 -0500

    commit desde la rama 1

--- persona.js
+++ persona.js
@@ -8,5 +8,8 @@ class Persona{
 
 p1 = new Persona('Luis', 15)
 p2 = new Persona('Sandra', 25)
+p3 = new Persona('Adry', 55)
 
 console.log(p1.nombre, p1.edad)
+console.log(p2.nombre, p2.edad)
+console.log(p3.nombre, p3.edad)
